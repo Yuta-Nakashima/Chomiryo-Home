cfca93af2b81a99213c6babf78b2dfdcfa3ba3a9
Yuta-Nakashima <33017398+Yuta-Nakashima@users.noreply.github.com>
2019-07-05 13:49:12 +0900

    Update server.py

--- 201906-e-team-master/server.py
+++ 201906-e-team-master/server.py
@@ -4,53 +4,208 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 import mysql.connector
+import numpy as np
+import cgi
+import sys
+import io
+
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         query = parse_qs(parsed_path.query)
         # print(parsed_path.path)
-        if "/choumiryou/history" in parsed_path.path:
+        if "/history" in parsed_path.path:
+            response = {}
             host, port = importargs()
+            text = parsed_path.path.split("/")
+            cname = text[2] 
             # ここにhistory呼び出し
-            response = {}
-        elif "/choumiryou/all_name" in parsed_path.path:
-            host, port = importargs()
-            # ここは全ての名前
-            response = {}
-        elif "/choumiryou" in parsed_path.path:
-            host, port = importargs()
-            # choumiryou table の情報
-            response = {}
-        else:
-            url = urlparse('mysql://root:eteam1206@localhost:3306/acahara')
+            url = urlparse('mysql://student:eteam1@localhost:3306/acahara')
             conn = mysql.connector.connect(
             host = url.hostname or 'localhost',
             port = url.port or 3306,
-            user = url.username or 'root',
+            user = url.username or 'student',
             password = url.password or '',
             database = url.path[1:],
             )
             conn.is_connected()
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM history where name = "'+cname+'"')
+            result = cur.fetchall()
+            cnt = 0
+            for i in result:
+                cnt = cnt + 1
+                res = {
+                    cnt: {
+                        'id': i[0],
+                        'name': i[1],
+                        'nokori': i[2],
+                        'shiyou': i[3],
+                        'koushin': i[4],
+                        'rireki': i[5]
+                    }
+                }
+                response.update(res)
+        elif "/login" in parsed_path.path:
+            response = {}
+            host, port = importargs()
+            # ここにhistory呼び出し
+            text = parsed_path.path.split("/")
+            usname = text[2]
+            url = urlparse('mysql://student:eteam1@localhost:3306/acahara')
+            conn = mysql.connector.connect(
+                host=url.hostname or 'localhost',
+                port=url.port or 3306,
+                user=url.username or 'student',
+                password=url.password or '',
+                database=url.path[1:],
+            )
+            conn.is_connected()
+            cur = conn.cursor()
+            cur.execute('SELECT pass FROM user where name = '+'"'+usname+'"')
+            result = cur.fetchall()
+            cnt = 0
+            for i in result:
+                cnt = cnt + 1
+                res = {
+                    cnt: {
+                        'pass': i[0]
+                    }
+                }
+                response.update(res)
+        elif "/choumiryou/all_name" in parsed_path.path:
+            response = {}
+            host, port = importargs()
+            # ここにhistory呼び出し
+            url = urlparse('mysql://student:eteam1@localhost:3306/acahara')
+            conn = mysql.connector.connect(
+                host=url.hostname or 'localhost',
+                port=url.port or 3306,
+                user=url.username or 'student',
+                password=url.password or '',
+                database=url.path[1:],
+            )
+            conn.is_connected()
+            cur = conn.cursor()
+            cur.execute('SELECT name FROM choumiryou')
+            result = cur.fetchall()
+            cnt = 0
+            for i in result:
+                cnt = cnt + 1
+                res = {
+                    cnt: {
+                        'name': i[0]
+                    }
+                }
+                response.update(res)
+        elif "/choumiryou" in parsed_path.path:
+            response = {}
+            host, port = importargs()
+            # ここにhistory呼び出し
+            url = urlparse('mysql://student:eteam1@localhost:3306/acahara')
+            conn = mysql.connector.connect(
+                host=url.hostname or 'localhost',
+                port=url.port or 3306,
+                user=url.username or 'student',
+                password=url.password or '',
+                database=url.path[1:],
+            )
+            conn.is_connected()
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM choumiryou')
+            result = cur.fetchall()
+            cnt = 0
+            for i in result:
+                cnt = cnt + 1
+                res = {
+                    cnt: {
+                        'id': i[0],
+                        'name': i[1],
+                        'nokori': i[2],
+                        'shiyou': i[3],
+                        'koushin': i[4],
+                        'rireki': i[5]
+                    }
+                }
+                response.update(res)
+
+        else:
+            response = {}
+            url = urlparse('mysql://student:eteam1@localhost:3306/acahara')
+            conn = mysql.connector.connect(
+            host = url.hostname or 'localhost',
+            port = url.port or 3306,
+            user = url.username or 'student',
+            password = url.password or '',
+            database = url.path[1:],
+            )
+            conn.is_connected()
+            cur.execute('SELECT * FROM choumiryou')
+            result = cur.fetchall()
+            cnt = 0
+            for i in result:
+                cnt = cnt + 1
+                res = {
+                    cnt: {
+                        'id': i[0],
+                        'name': i[1],
+                        'nokori': i[2],
+                        'shiyou': i[3],
+                        'koushin': i[4],
+                        'rireki': i[5]
+                    }
+                }
+                response.update(res)
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         responseBody = json.dumps(response)
         self.wfile.write(responseBody.encode('utf-8'))
 
     def do_POST(self):
         try:
+            parsed_path = urlparse(self.path)
             content_len = int(self.headers.get('content-length'))
             requestBody = json.loads(self.rfile.read(content_len).decode('utf-8'))
             print(requestBody)
-
             response = {'result': True}
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
             responseBody = json.dumps(response)
             # ここにPOSTの処理を加える
+            url = urlparse('mysql://student:eteam1@localhost:3306/acahara')
+            conn = mysql.connector.connect(
+            host = url.hostname or 'localhost',
+            port = url.port or 3306,
+            user = url.username or 'student',
+            password = url.password or '',
+            database = url.path[1:],
+            )
+            if "/user" in parsed_path.path:
+                #user point add
+                cur = conn.cursor()
+                text =parsed_path.path.split("/")
+                uname = text[2]
+                upoint = text[3]
+                cur.execute('update user set point ="'+upoint+'" where name = "'+uname+'"')
+                conn.commit()
+
+            elif "/choumiryou" in parsed_path.path:
+                cur = conn.cursor()
+                cur.execute('SELECT * FROM choumiryou WHERE name = "shouyu"')
+                result = cur.fetchall()
+                cur.execute('insert into history(name, nokori, shiyou, koushin, rireki) values("'+result[0][1]+'","'+result[0][2]+'","'+result[0][3]+'","'+result[0][4]+'","'+result[0][5]+'")')
+                sql = 'update choumiryou set nokori ="'+requestBody['nokori']+'",koushin = "'+requestBody['koushin']+'",shiyou ="'+requestBody['shiyou']+'" where name = "shouyu"'
+                print(sql)
+                cur.execute(sql)
+                conn.commit()
+
+
 
             self.wfile.write(responseBody.encode('utf-8'))
         except Exception as e:
@@ -71,18 +226,21 @@ class MyHandler(BaseHTTPRequestHandler):
 
 
 
+
+
 def importargs():
     parser = argparse.ArgumentParser("This is the simple server")
 
     parser.add_argument('--host', '-H', required=False, default='0.0.0.0')
-    parser.add_argument('--port', '-P', required=False, type=int, default=8080)
+    parser.add_argument('--port', '-P', required=False, type=int, default=8000)
+
 
     args = parser.parse_args()
 
     return args.host, args.port
 
 
-def run(server_class=HTTPServer, handler_class=MyHandler, server_name='localhost', port=8080):
+def run(server_class=HTTPServer, handler_class=MyHandler, server_name='localhost', port=8000):
     server = server_class((server_name, port), handler_class)
     server.serve_forever()
 
